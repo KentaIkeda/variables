016e960005c8f968ed70c67662c5f41a86ca5b91
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-28 21:14:04 +0900

    clear cache

--- target/debug/deps/variables-b3c04075f2606355.d
+++
@@ -1,5 +0,0 @@
-C:\Users\airon\Desktop\rust\variables\target\debug\deps\libvariables-b3c04075f2606355.rmeta: src/main.rs
-
-C:\Users\airon\Desktop\rust\variables\target\debug\deps\variables-b3c04075f2606355.d: src/main.rs
-
-src/main.rs:
